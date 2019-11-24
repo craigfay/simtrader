8cb91f81809f9b34a1f2e688ca70ccdf986ae099
craig fay <craigfay1@gmail.com>
2019-11-23 20:20:47 -0600

    Add singularize table names

--- services/database/migrations/00-public-tables.sql
+++ services/database/migrations/00-public-tables.sql
@@ -2,43 +2,43 @@
 
 \connect db;
 
-create table public.actors (
+create table public.actor (
     id serial primary key,
     cash float(2) not null default 0.00,
     created_date timestamp default current_timestamp,
     updated_date timestamp default current_timestamp
 );
 
-comment on table public.actors IS
+comment on table public.actor IS
 'Actors represent people/programs that interact with the simulator';
 
-create table public.actions (
+create table public.action (
     id serial primary key,
     name text
 );
 
-INSERT INTO public.actions(name) VALUES ('BUY'), ('SELL');
+INSERT INTO public.action(name) VALUES ('BUY'), ('SELL');
 
-comment on table public.actions IS
+comment on table public.action IS
 'Actions is a reference table that defines the available types of transactions';
 
-create table public.transactions (
+create table public.transaction (
     id serial primary key,
     symbol text not null,
     quantity integer not null,
     price float(2) not null,
     created_date timestamp default current_timestamp,
     updated_date timestamp default current_timestamp,
-    action integer not null references public.actions(id),
-    actor_id integer not null references public.actors(id)
+    action integer not null references public.action(id),
+    actor_id integer not null references public.actor(id)
 );
 
-create table public.positions (
+create table public.position (
     id serial primary key,
     symbol text not null,
     quantity integer not null,
-    actor_id integer not null references public.actors(id)
+    actor_id integer not null references public.actor(id)
 );
 
-comment on table public.transactions is
+comment on table public.transaction is
 'Transactions represent asset trades by a particular actor';
